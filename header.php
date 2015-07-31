2697e41af8e6583c65b6c50794176f8c00c4a029
abdullahashik <abdullahilashik@gmail.com>
2015-07-31 13:27:05 +0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Simple Login & Registration</title>
+    <link href="css/bootstrap.css" rel="stylesheet"/>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery-2.1.1.min.js"></script>
+</head>
+<body>
+<div class="container">
